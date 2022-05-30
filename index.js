bb4216fc49e9a9b0b482ee795bfcfc72cea0cba9
unknown <rsvaghasiya0@gmail.com>
2022-05-30 08:48:06 +0530

    Added testing route

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("Home page!");
+});
+app.get("/login", (req, res) => {
+  console.log(req);
+  res.send("Login page!");
+});
+
+app.get("/signup", (req, res) => {
+  console.log(req);
+  res.send("signup page!");
+});
+
+app.get("/logout", (req, res) => {
+  console.log(req);
+  res.send("Signout page!");
+});
+const admin = (req, res) => {
+  console.log(req);
+  res.send("admin page!");
+};
+
+const isAdmin = (req, res, next) => {
+  console.log(req);
+  res.send("Admin dashboard page!");
+  next();
+};
+app.get("/admin", admin, isAdmin);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
