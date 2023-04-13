309a4bda451c24739dd7a44a6589e971da2fe5e0
Ronak Vaghasiya <rsvaghasiya0@gmail.com>
2023-04-13 23:03:21 +0530

    Added get user and update user

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 //DB Connection
 mongoose
@@ -27,6 +28,7 @@ app.use(cors());
 
 //My Routes
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require('../models/user')
 const { check, validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
-var { expressjwt } = require("express-jwt");
+var { expressjwt } = require('express-jwt')
 
 exports.signup = (req, res) => {
     const errors = validationResult(req)
@@ -77,7 +77,7 @@ exports.isSignedin = expressjwt({
 
 //custom middlewares
 exports.isAuthenticated = (req, res, next) => {
-    let checker = req.profile && req.auth && req.profile._id === req.auth._id
+    let checker = req.profile && req.auth && req.profile._id == req.auth._id
     if (checker) {
         return res.status(403).json({
             error: 'access denied',
